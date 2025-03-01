612bea03f0168c6612b4a971ae87108d2a3dcf24
MasnunRez <masnun.rezon93@gmail.com>
2025-03-02 01:08:10 +0600

    Body background color changes one by one

--- index.js
+++ index.js
@@ -7,184 +7,197 @@ document.getElementById("big-button").addEventListener("click", function () {
 document
   .getElementById("btn-completed-1")
   .addEventListener("click", function () {
-    // Button disabled after clicked 
-    document.getElementById('btn-completed-1').disabled = true;
+    // Button disabled after clicked
+    document.getElementById("btn-completed-1").disabled = true;
     //   console.log("Button working");
-    // Alert show 
+    // Alert show
     alert("Click OK to Complete the Task");
-    // Add history 
+    // Add history
     const secondList = document.getElementById("history");
     const list = document.createElement("list");
     const getDate = new Date();
     const currentDate = getDate.toLocaleString("en-BD");
     list.innerHTML = `<p>You have completed the Task on</p>` + currentDate;
     secondList.appendChild(list);
-    // Minus a number ............  
-    let number = document.getElementById('task-number')
+    // Minus a number ............
+    let number = document.getElementById("task-number");
     let convertedNum = parseInt(number.innerText);
     // console.log(typeof convertedNum)
-    let numberNav = document.getElementById('nav-number')
+    let numberNav = document.getElementById("nav-number");
     let convertedNum2 = parseInt(numberNav.innerText);
 
-    calcNumber = convertedNum-1;
-    calcNumber2 = convertedNum2+1;
+    calcNumber = convertedNum - 1;
+    calcNumber2 = convertedNum2 + 1;
     number.innerText = calcNumber.toString().padStart(2, "0");
     numberNav.innerText = calcNumber2.toString().padStart(2, "0");
-});
-
+  });
 
 // Completed Button 2 ...............
 
 document
   .getElementById("btn-completed-2")
   .addEventListener("click", function () {
-    // Button disabled after clicked 
-    document.getElementById('btn-completed-2').disabled = true;
+    // Button disabled after clicked
+    document.getElementById("btn-completed-2").disabled = true;
     //   console.log("Button working");
-    // Alert show 
+    // Alert show
     alert("Click OK to Complete the Task");
-    // Add history 
+    // Add history
     const secondList = document.getElementById("history");
     const list = document.createElement("list");
     const getDate = new Date();
     const currentDate = getDate.toLocaleString("en-BD");
     list.innerHTML = `<p>You have completed the Task on</p>` + currentDate;
     secondList.appendChild(list);
-    // Minus a number ............  
-    let number = document.getElementById('task-number')
+    // Minus a number ............
+    let number = document.getElementById("task-number");
     let convertedNum = parseInt(number.innerText);
     // console.log(typeof convertedNum)
-    let numberNav = document.getElementById('nav-number')
+    let numberNav = document.getElementById("nav-number");
     let convertedNum2 = parseInt(numberNav.innerText);
 
-    calcNumber = convertedNum-1;
-    calcNumber2 = convertedNum2+1;
+    calcNumber = convertedNum - 1;
+    calcNumber2 = convertedNum2 + 1;
     number.innerText = calcNumber.toString().padStart(2, "0");
     numberNav.innerText = calcNumber2.toString().padStart(2, "0");
-});
+  });
 
 // Completed Button 3 ...............
 document
   .getElementById("btn-completed-3")
   .addEventListener("click", function () {
-    // Button disabled after clicked 
-    document.getElementById('btn-completed-3').disabled = true;
+    // Button disabled after clicked
+    document.getElementById("btn-completed-3").disabled = true;
     //   console.log("Button working");
-    // Alert show 
+    // Alert show
     alert("Click OK to Complete the Task");
-    // Add history 
+    // Add history
     const secondList = document.getElementById("history");
     const list = document.createElement("list");
     const getDate = new Date();
     const currentDate = getDate.toLocaleString("en-BD");
     list.innerHTML = `<p>You have completed the Task on</p>` + currentDate;
     secondList.appendChild(list);
-    // Minus a number ............  
-    let number = document.getElementById('task-number')
+    // Minus a number ............
+    let number = document.getElementById("task-number");
     let convertedNum = parseInt(number.innerText);
     // console.log(typeof convertedNum)
-    let numberNav = document.getElementById('nav-number')
+    let numberNav = document.getElementById("nav-number");
     let convertedNum2 = parseInt(numberNav.innerText);
 
-    calcNumber = convertedNum-1;
-    calcNumber2 = convertedNum2+1;
+    calcNumber = convertedNum - 1;
+    calcNumber2 = convertedNum2 + 1;
     number.innerText = calcNumber.toString().padStart(2, "0");
     numberNav.innerText = calcNumber2.toString().padStart(2, "0");
-});
+  });
 
 // Completed Button 4 ...............
 document
   .getElementById("btn-completed-4")
   .addEventListener("click", function () {
-    // Button disabled after clicked 
-    document.getElementById('btn-completed-4').disabled = true;
+    // Button disabled after clicked
+    document.getElementById("btn-completed-4").disabled = true;
     //   console.log("Button working");
-    // Alert show 
+    // Alert show
     alert("Click OK to Complete the Task");
-    // Add history 
+    // Add history
     const secondList = document.getElementById("history");
     const list = document.createElement("list");
     const getDate = new Date();
     const currentDate = getDate.toLocaleString("en-BD");
     list.innerHTML = `<p>You have completed the Task on</p>` + currentDate;
     secondList.appendChild(list);
-    // Minus a number ............  
-    let number = document.getElementById('task-number')
+    // Minus a number ............
+    let number = document.getElementById("task-number");
     let convertedNum = parseInt(number.innerText);
     // console.log(typeof convertedNum)
-    let numberNav = document.getElementById('nav-number')
+    let numberNav = document.getElementById("nav-number");
     let convertedNum2 = parseInt(numberNav.innerText);
 
-    calcNumber = convertedNum-1;
-    calcNumber2 = convertedNum2+1;
+    calcNumber = convertedNum - 1;
+    calcNumber2 = convertedNum2 + 1;
     number.innerText = calcNumber.toString().padStart(2, "0");
     numberNav.innerText = calcNumber2.toString().padStart(2, "0");
-});
+  });
 
 // Completed Button 5 ...............
 document
   .getElementById("btn-completed-5")
   .addEventListener("click", function () {
-    // Button disabled after clicked 
-    document.getElementById('btn-completed-5').disabled = true;
+    // Button disabled after clicked
+    document.getElementById("btn-completed-5").disabled = true;
     //   console.log("Button working");
-    // Alert show 
+    // Alert show
     alert("Click OK to Complete the Task");
-    // Add history 
+    // Add history
     const secondList = document.getElementById("history");
     const list = document.createElement("list");
     const getDate = new Date();
     const currentDate = getDate.toLocaleString("en-BD");
     list.innerHTML = `<p>You have completed the Task on</p>` + currentDate;
     secondList.appendChild(list);
-    // Minus a number ............  
-    let number = document.getElementById('task-number')
+    // Minus a number ............
+    let number = document.getElementById("task-number");
     let convertedNum = parseInt(number.innerText);
     // console.log(typeof convertedNum)
-    let numberNav = document.getElementById('nav-number')
+    let numberNav = document.getElementById("nav-number");
     let convertedNum2 = parseInt(numberNav.innerText);
 
-    calcNumber = convertedNum-1;
-    calcNumber2 = convertedNum2+1;
+    calcNumber = convertedNum - 1;
+    calcNumber2 = convertedNum2 + 1;
     number.innerText = calcNumber.toString().padStart(2, "0");
     numberNav.innerText = calcNumber2.toString().padStart(2, "0");
-});
+  });
 
 // Completed Button 6 ...............
 
 document
   .getElementById("btn-completed-6")
   .addEventListener("click", function () {
-    // Button disabled after clicked 
-    document.getElementById('btn-completed-6').disabled = true;
+    // Button disabled after clicked
+    document.getElementById("btn-completed-6").disabled = true;
     //   console.log("Button working");
-    // Alert show 
+    // Alert show
     alert("Click OK to Complete the Task");
-    // Add history 
+    // Add history
     const secondList = document.getElementById("history");
     const list = document.createElement("list");
     const getDate = new Date();
     const currentDate = getDate.toLocaleString("en-BD");
     list.innerHTML = `<p>You have completed the Task on</p>` + currentDate;
     secondList.appendChild(list);
-    // Minus a number ............  
-    let number = document.getElementById('task-number')
+    // Minus a number ............
+    let number = document.getElementById("task-number");
     let convertedNum = parseInt(number.innerText);
     // console.log(typeof convertedNum)
-    let numberNav = document.getElementById('nav-number')
+    let numberNav = document.getElementById("nav-number");
     let convertedNum2 = parseInt(numberNav.innerText);
 
-    calcNumber = convertedNum-1;
-    calcNumber2 = convertedNum2+1;
+    calcNumber = convertedNum - 1;
+    calcNumber2 = convertedNum2 + 1;
     number.innerText = calcNumber.toString().padStart(2, "0");
     numberNav.innerText = calcNumber2.toString().padStart(2, "0");
-});
+  });
 
+// let bgColors = false
 
+// function colorChange(){
+//     document.body.style.backgroundColor = bgColors ? 'black' : 'red';
+//     bgColors = !bgColors;
+// }
 
+// Change body background
+function colorChange() {
+  let bodyColor = document.body.style.backgroundColor;
 
-
-
+  if (bodyColor === "black") {
+    document.body.style.backgroundColor = "orange";
+  } else if (bodyColor === "orange") {
+    document.body.style.backgroundColor = "rgb(244, 247, 255)";
+  } else {
+    document.body.style.backgroundColor = "black";
+  }
+}
 
 // Clear History
 document.getElementById("clear-history").addEventListener("click", function () {
