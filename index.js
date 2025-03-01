01ca6bed7bddcb70512afd5f0c4cfaa5d5712e04
MasnunRez <masnun.rezon93@gmail.com>
2025-03-01 21:35:28 +0600

    Task assigned -1 after clickd done

--- index.js
+++ index.js
@@ -7,20 +7,41 @@ document.getElementById("big-button").addEventListener("click", function () {
 document
   .getElementById("btn-completed-1")
   .addEventListener("click", function () {
+    // Button disabled after clicked 
     document.getElementById('btn-completed-1').disabled = true;
     //   console.log("Button working");
+    // Alert show 
     alert("Click OK to Complete the Task");
+    // Add history 
     const secondList = document.getElementById("history");
     const list = document.createElement("list");
-
     const getDate = new Date();
     const currentDate = getDate.toLocaleString("en-BD");
     list.innerHTML = `<p>You have completed the Task on</p>` + currentDate;
     secondList.appendChild(list);
-  });
-// function clickedDisabled(){
-//     document.getElementById('btn-completed-1').disabled = true;
-// }
+    // Minus a number ............  
+    let number = document.getElementById('task-number')
+    let convertedNum = parseInt(number.innerText);
+    // console.log(typeof convertedNum)
+    calcNumber = convertedNum-1;
+    number.innerText = calcNumber.toString().padStart(2, "0");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
